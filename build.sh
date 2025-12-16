16b5d39dde928ba957a1dd88ad263b5eb655a522
Sergey Boykov <sergsv03@gmail.com>
2025-12-16 21:24:53 +0100

    zmk v0.4 upgrade

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # Builds firmware in Dev Container environment
 cd /workspaces/zmk/app || exit
-west build -d build/sofle_left -b nice_nano_v2 -- -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config/zmk-userspace" -DSHIELD="sofle_left"  
-west build -d build/sofle_right -b nice_nano_v2 -- -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config/zmk-userspace" -DSHIELD="sofle_right"
+west build -d build/sofle_left -b nice_nano -- -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config/zmk-userspace;/workspaces/zmk-config/zmk-helpers" -DSHIELD="sofle_left"  
+west build -d build/sofle_right -b nice_nano -- -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config/zmk-userspace;/workspaces/zmk-config/zmk-helpers" -DSHIELD="sofle_right"
 mkdir -p /workspaces/zmk-config/out/
 cp build/sofle_left/zephyr/zmk.uf2 /workspaces/zmk-config/out/sofle_left.uf2
 cp build/sofle_right/zephyr/zmk.uf2 /workspaces/zmk-config/out/sofle_right.uf2
